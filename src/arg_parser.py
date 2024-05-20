743ba03a46a23e9b743be830db5f0c2deef6862f
Eros Mendonça <erosmendonca@outlook.com>
2024-05-20 23:40:59 +0100

    feat: arg to output

--- src/arg_parser.py
+++ src/arg_parser.py
@@ -3,5 +3,10 @@ import argparse
 def init_arg_parser():
   parser = argparse.ArgumentParser(description='Apply filters to images using OpenCV.')
   parser.add_argument('--path', type=str, required=True, help='Path to the input image.')
+  # parser.add_argument('--filter', type=str, required=True, choices=['gaussian', 'median', 'bilateral'], help='Filter to apply to the image.')
+  parser.add_argument('--kernel', type=int, nargs='+', help='Kernel size for the filter. For example, pass --kernel 15 15 for a 15x15 kernel.')
+  parser.add_argument('--sigma-color', type=float, help='Sigma color for the bilateral filter.')
+  parser.add_argument('--sigma-space', type=float, help='Sigma space for the bilateral filter.')
+  parser.add_argument('--output', type=str, help='Path to save the output image.')
 
   return parser
--- src/main.py
+++ src/main.py
@@ -12,11 +12,10 @@ else:
   # Apply Gaussian blur with a kernel size of (15, 15)
   blurred_image = cv2.GaussianBlur(image, (15, 15), 0)
 
-  # Display the original and blurred images
-  cv2.imshow('Original Image', image)
-  cv2.imshow('Blurred Image', blurred_image)
+  # Save the output image
+  if parser.output:
+    cv2.imwrite(parser.output, blurred_image)
+  else:
+    cv2.imwrite('output.jpg', blurred_image)
 
-  # Wait for a key press and close the image windows
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
   
